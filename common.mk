a1e8b91316d77a36db74501680747de8c4851f9e
Vince Tse <vincetse@users.noreply.github.com>
2020-10-21 09:33:39 -0400

    Appropriate name for Swarm manager

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
-host = zero00
-docker_host = DOCKER_HOST=ssh://vtse@$(host) DOCKER_TIMEOUT=300
+manager_host = zero00
+docker_host = DOCKER_HOST=ssh://vtse@$(manager_host) DOCKER_TIMEOUT=300
 docker = $(docker_host) docker
 stack = $(docker) stack
 service = $(docker) service
