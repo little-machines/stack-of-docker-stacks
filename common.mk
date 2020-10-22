80f91a290cab026aedaff9d72ebc2459af4749c0
Vince Tse <vincetse@users.noreply.github.com>
2020-10-22 00:10:59 -0400

    Parameterize and centralize service port listing

--- common.mk
+++ common.mk
@@ -3,3 +3,7 @@ docker_host = DOCKER_HOST=ssh://vtse@$(manager_host) DOCKER_TIMEOUT=300
 docker = $(docker_host) docker
 stack = $(docker) stack
 service = $(docker) service
+
+# port assignments
+hello_world_api_port = 8001
+todos_api_port = 3000
